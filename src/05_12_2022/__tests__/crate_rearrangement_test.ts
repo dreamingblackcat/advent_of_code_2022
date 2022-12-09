8a6f9146681821c2677700ca2c604f592f0b62e6
dreamingblackcat <dreamingblackcat10@gmail.com>
2022-12-09 11:33:27 +0900

    Day 5: part 1 solved

--- src/05_12_2022/__tests__/crate_rearrangement_test.ts
+++ src/05_12_2022/__tests__/crate_rearrangement_test.ts
@@ -1,22 +1,74 @@
 import * as jest from 'jest';
-import { rangeOverlapCheck, Range, countContainedPairs, countOverlappedPairs } from '../cleaning_assignment';
+import { moveCrate, MoveInstruction, State, parseState, runInstructions  } from '../crate_rearrangement';
 
-describe('Cleaning Assignments', () => {
+describe('Crate rearrangements', () => {
 
   describe('moveCrate', () => {
     it("updates crate state.", () => {
-      let crateState = {
+      let crateState: State = {
         1: ["A", "B"],
         2: [],
       }
 
-      let expectedState = {
+      let expectedState: State = {
         1: [],
-        2: ["A", "B"]
+        2: ["B", "A"]
       }
 
-      expect(moveCrate(2, 1, 2)).toEqual(false);
-      expect(rangeOverlapCheck(range2, range1)).toEqual(true);
+      let instruction1: MoveInstruction = {
+        from: 1,
+        to: 2,
+        crates: 2,
+      }
+
+      expect(moveCrate(crateState, instruction1)).toMatchObject(expectedState);
+    })
+  })
+
+  describe('runInstructions', () => {
+    it("parses sample input correctly.", () => {
+      let sampleState = {
+          1: ["Z", "N"],
+          2: ["M", "C", "D"],
+          3: ["P"]
+      }
+      //move 1 from 2 to 1
+      //move 3 from 1 to 3
+      //move 2 from 2 to 1
+      //move 1 from 1 to 2
+      let instructions = [
+        { crates: 1, from: 2, to: 1 },
+        { crates: 3, from: 1, to: 3},
+        { crates: 2, from: 2, to: 1},
+        { crates: 1, from: 1, to: 2},
+      ]
+
+      console.log(runInstructions(sampleState, instructions))
+      expect(runInstructions(sampleState, instructions)).toMatchObject(
+        {
+          1: ["C"],
+          2: ["M"],
+          3: ["P", "D", "N", "Z"]
+        }
+      )
+    })
+  })
+
+  describe('parseState', () => {
+    fit("parses sample input correctly.", () => {
+      let sampleInput = [
+        "[D]        ",
+        "[N] [C]    ",
+        "[Z] [M] [P]",
+      ];
+
+      expect(parseState(sampleInput)).toMatchObject(
+        {
+          1: ["Z", "N", "D"],
+          2: ["M", "C"],
+          3: ["P"]
+        }
+      )
     })
   })
 })
---
+++ src/05_12_2022/crate_rearrangement.ts
@@ -0,0 +1,125 @@
+import take from 'lodash/fp/take';
+import takeRight from 'lodash/fp/takeRight';
+import chunk from 'lodash/fp/chunk';
+import reduce from 'lodash/fp/reduce';
+import last from 'lodash/fp/last';
+import reverse from 'lodash/fp/reverse';
+import pipe from 'lodash/fp/pipe';
+import map from 'lodash/fp/map';
+import unzip from 'lodash/fp/unzip';
+import compact from 'lodash/fp/compact';
+
+import readAocInput from '../read_input';
+
+export type MoveInstruction = {
+  from: number,
+  to: number,
+  crates: number,
+};
+
+export type State = {
+  [key: number]: string[],
+};
+
+export const parseState = (stateLines: string[]): State => {
+  // TODO: implement a limited parser here later.
+  let stacksCount = pipe(
+    last,
+    chunk(4),
+    last,
+    (str: string[]) => Number(str[1])
+  )(stateLines)
+
+  let extractItem = (line: string) => {
+    return pipe(
+      chunk(4),
+      map((chunks) => chunks[1]),
+    )(line)
+  };
+
+  let items = pipe(
+    map(extractItem),//only get item inside the boxes [X] -> 'X'
+    unzip, // lodash unzip transpose an array
+    map((ary) => ary.filter(item => item != ' ')),// transpose array is not properly compacted, so compacting here
+    map(reverse), // And each array from the transposed row is in reverse order. So, filpping them back.
+    map(compact) // And each array from the transposed row is in reverse order. So, filpping them back.
+  )(stateLines)
+  let state: State = {};
+  items.map((ary, idx) => {
+    state[idx+1] = ary
+  })
+  return state;
+};
+
+const parseInstructions = (instructionLines: string[]): MoveInstruction[] => {
+  let ins: Array<MoveInstruction | null> = map((line: string) => {
+    let match = line.match(/move (\d+) from (\d+) to (\d+)/);
+    if (match) {
+      return {
+        crates: Number(match[1]),
+        from: Number(match[2]),
+        to: Number(match[3]),
+      }
+    } else {
+      return null;
+    };
+  })(instructionLines);
+
+  return compact(ins);
+}
+
+
+export const moveCrate = (state: State, instruction: MoveInstruction): State => {
+  let fromList = state[instruction.from];
+  let toList = state[instruction.to];
+
+  let newState = {
+    ...state,
+    [instruction.from]: take(fromList.length - instruction.crates)(fromList),
+    [instruction.to]: toList.concat(reverse(takeRight(instruction.crates)(fromList))),
+  }
+
+  return newState;
+}
+
+const debug = (func: (...args: any[]) => any) => {
+  return (...args: any[]) => {
+    console.log("Arguments:", args);
+    let result = func.apply(null, args);
+    console.log("Result:", result);
+    return result;
+  }
+}
+
+export const runInstructions = (state: State, instructions: MoveInstruction[]): State => {
+  return reduce(moveCrate)(state)(instructions);
+}
+
+//[N]         [C]     [Z]
+//[Q] [G]     [V]     [S]         [V]
+//[L] [C]     [M]     [T]     [W] [L]
+//[S] [H]     [L]     [C] [D] [H] [S]
+//[C] [V] [F] [D]     [D] [B] [Q] [F]
+//[Z] [T] [Z] [T] [C] [J] [G] [S] [Q]
+//[P] [P] [C] [W] [W] [F] [W] [J] [C]
+//[T] [L] [D] [G] [P] [P] [V] [N] [R]
+// 1   2   3   4   5   6   7   8   9
+const input1State = {
+
+}
+
+
+if (require.main === module) {
+  readAocInput(__dirname, "input1.txt", (lines: Array<string>) => {
+    let stateLines = lines.filter(line => line.match(/\[/))
+    let initialState = parseState(stateLines);
+    const ins = parseInstructions(lines);
+    console.log(ins)
+    console.log(initialState)
+
+    const message = Object.values(runInstructions(initialState, ins)).map(ary => last(ary)).join("")
+
+    console.log(`Part 1: Message for the elves: ${message}`)
+  })
+
+}
